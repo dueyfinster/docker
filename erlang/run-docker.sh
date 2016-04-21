7a1f188450c906f2a1f9989135d7c0d2b607deeb
Neil Grogan <neil@ngrogan.com>
2016-04-21 17:52:16 +0100

    Removed all from Java branch except java

--- erlang/run-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-docker pull dueyfinster/lang:erlang
-
-docker run --rm=true -ti -v $DIR:/data --name erlang dueyfinster/lang:erlang
--- node/run-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker pull dueyfinster/lang:node
-
-docker run --rm=true -ti -v $DIR:/data --name nodejs dueyfinster/lang:node
