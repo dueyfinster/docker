645837affb2583368c616baee8bbadee2e54104f
Neil Grogan <neil@ngrogan.com>
2016-04-21 18:14:48 +0100

    Removed all from Go branch except Go

--- python/run-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker pull dueyfinster/lang:python
-
-docker run --rm=true -ti -v $DIR:/data --name python dueyfinster/lang:python
